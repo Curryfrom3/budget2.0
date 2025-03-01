8bd49896297060b241a4d0eb9ba32c28f38c7ed9
Deon Curry <deoncvn68@gmail.com>
2025-03-01 11:17:16 -0500

    fixed schema

--- models/user.js
+++ models/user.js
@@ -1,22 +1,31 @@
 const mongoose = require('mongoose');
 
+const mongoose = require('mongoose');
+
 const transactionSchema = new mongoose.Schema({
-  catagory: {
-    type: String,
-    required: true,
-  },
-  amount: {
-    type: Number,
-    required: true,
-  },
-  description: {
-    type: String,
-  },
-  date: {
-    type: Date,
-  },
-  
+
+  name: { type: String, 
+    required: true
+   }, // Name of transaction
+
+  amount: { type: Number, 
+    required: true 
+  }, // Dollar amount
+
+  category: { type: String,
+     enum: ['income', 'food', 'transportation', 'entertainment', 'bills', 'savings', 'other'], 
+     required: true 
+    }, // Spending category
+
+  type: { type: String,
+     enum: ['income', 'expense'], 
+     required: true 
+    }, // Transaction type
+
+  date: { type: Date, required: true }, // Date of transaction
 });
+
+module.exports = mongoose.model('Transaction', transactionSchema);
 
 const userSchema = mongoose.Schema({
   username: {
